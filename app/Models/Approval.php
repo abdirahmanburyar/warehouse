baf60bb97097ab1cd6b28685cfaf2518f28a9701
Dtechdevelopment <dtechsolutionsco@gmail.com>
2025-03-24 06:48:16 +0300

    fixed some issues

--- app/Models/Approval.php
+++ app/Models/Approval.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\Role;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Approval extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'role_id',
         'activity_type',
