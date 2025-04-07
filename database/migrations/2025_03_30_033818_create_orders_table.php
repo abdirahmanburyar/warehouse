524e279a80bb19ea051e23762a6e3b0964420cd9
Dtechdevelopment <dtechsolutionsco@gmail.com>
2025-04-07 22:40:39 +0300

    001

--- database/migrations/2025_03_30_033818_create_orders_table.php
+++ database/migrations/2025_03_30_033818_create_orders_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('warehouse_id')->constrained()->onDelete('cascade');
-            $table->foreignId('facility_id')->constrained()->onDelete('cascade');
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('warehouse_id')->onDelete('cascade');
+            $table->foreignId('facility_id')->onDelete('cascade');
+            $table->foreignId('user_id')->onDelete('cascade');
             $table->string('order_number')->unique();
             $table->enum('status', ['pending', 'approved', 'rejected', 'in processing', 'dispatched', 'delivered'])->default('pending');
             $table->integer('number_items');
