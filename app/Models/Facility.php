1124f583ad155f56ac7177df07056a7add59ab3d
Dtechdevelopment <dtechsolutionsco@gmail.com>
2025-04-10 05:44:15 +0300

    removed softdelete

--- app/Models/Facility.php
+++ app/Models/Facility.php
@@ -3,13 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use App\Models\User;
 
 class Facility extends Model
 {
-    use SoftDeletes;
 
     protected $fillable = [
         'name',
--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PurchaseOrder extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     protected $fillable = [
         'po_number',
