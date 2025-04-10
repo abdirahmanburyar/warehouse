d340169289f643834423549dacebc1f9ac3d5053
Dtechdevelopment <dtechsolutionsco@gmail.com>
2025-04-10 07:11:28 +0300

    fixed bugs

--- database/migrations/2025_04_04_003333_create_purchase_order_items_table.php
+++ database/migrations/2025_04_04_003333_create_purchase_order_items_table.php
@@ -17,20 +17,18 @@ return new class extends Migration
             $table->foreignId('packing_list_id')->nullable()->constrained('packing_lists')->onDelete('cascade');
 
             $table->foreignId('product_id')->constrained('products')->onDelete('restrict');
-            $table->string('item_code');
-            $table->string('item_description');
-            $table->string('uom')->nullable();
-            $table->integer('quantity')->default(0);
-            $table->integer('original_quantity')->default(0);
+            $table->foreignId('warehouse_id')->nullable()->constrained('warehouses')->onDelete('restrict');
             $table->string('location')->nullable();
+            $table->integer('quantity')->default(0);
             $table->integer('received_quantity')->default(0);
             $table->integer('damage_quantity')->default(0);
 
             $table->double('unit_cost')->default(0);
             $table->double('total_cost')->default(0);
 
-            $table->date('expiry_date')->nullable();
-            $table->string('batch_number')->nullable();
+            $table->date('expiry_date');
+            $table->string('batch_number');
+            $table->string('generic_name')->nullable();
 
             $table->string('status')->default('pending');
             $table->timestamps();
