850d9a3bd34402a0fd82609c6bfd1135cc792c72
abdirahmanburyar <buryar313@gmail.com>
2025-05-27 13:00:09 +0300

    Liquidate and disposals are done - v2

--- resources/js/Pages/Product/Dosage/Index.vue
+++ resources/js/Pages/Product/Dosage/Index.vue
@@ -4,9 +4,14 @@
         description="Manage product dosage forms"
     >
         <div class="flex justify-between items-center">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dosage Forms
-            </h2>
+            <div>
+                <Link :href="route('products.index')" class="inline-flex items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" /></svg>
+                    Back to Products
+                </Link>
+
+                <h2 class="font-semibold text-xl text-black leading-tight">Dosage Forms</h2>
+            </div>
             <Link
                 :href="route('products.dosages.create')"
                 class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
