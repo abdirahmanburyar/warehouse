7dd2ebd96f927b27b85cb9b8ab4618e544208168
abdirahmanburyar <buryar313@gmail.com>
2025-05-31 02:28:56 +0300

    user events are added

--- app/Events/TransferStatusChanged.php
+++ app/Events/TransferStatusChanged.php
@@ -16,16 +16,25 @@ class TransferStatusChanged implements ShouldBroadcast
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $transfer;
+    public $oldStatus;
+    public $newStatus;
+    public $changedBy;
 
     /**
      * Create a new event instance.
      *
      * @param  Transfer  $transfer
+     * @param  string  $oldStatus
+     * @param  string  $newStatus
+     * @param  int  $changedBy
      * @return void
      */
-    public function __construct(Transfer $transfer)
+    public function __construct(Transfer $transfer, string $oldStatus, string $newStatus, int $changedBy)
     {
         $this->transfer = $transfer;
+        $this->oldStatus = $oldStatus;
+        $this->newStatus = $newStatus;
+        $this->changedBy = $changedBy;
     }
 
     /**
