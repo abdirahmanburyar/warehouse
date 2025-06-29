db8929f7a198984cb2255215556c8360d040611e
abdirahmanburyar <buryar313@gmail.com>
2025-06-29 16:47:48 +0300

    po fixed

--- resources/js/Pages/Supplies/NewPo.vue
+++ resources/js/Pages/Supplies/NewPo.vue
@@ -1,7 +1,7 @@
 <template>
 
     <Head title="Purchase Order" />
-    <AuthenticatedLayout title="Purchase Orders" description="Manage your purchase orders">
+    <AuthenticatedLayout title="Purchase Orders" description="Manage your purchase orders" img="/assets/images/orders.png">
         <Link :href="route('supplies.index')" class="inline-flex items-center text-gray-600 hover:text-indigo-600 transition-colors duration-200 mb-6">
             <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
             Back to Purchase Orders
