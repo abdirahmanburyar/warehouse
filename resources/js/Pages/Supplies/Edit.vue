432763fe61898365ea8c41b52025eb2204d20309
abdirahmanburyar <buryar313@gmail.com>
2025-06-29 08:55:41 +0300

    po fixed

--- resources/js/Pages/Supplies/Edit.vue
+++ resources/js/Pages/Supplies/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-    <AuthenticatedLayout>
+    <AuthenticatedLayout title="Edit Supplier" description="Edit supplier details" img="/assets/images/orders.png">
         <div class="containe mx-auto py-6">
             <Link :href="route('supplies.index')" class="flex items-center text-gray-500 hover:text-gray-700">
                 <svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
