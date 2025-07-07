cdfaf2ea72451de7cf114a0a350e9923f036e509
abdirahmanburyar <buryar313@gmail.com>
2025-07-07 12:53:53 +0300

    eligible items

--- resources/js/Pages/Product/Eligible/Create.vue
+++ resources/js/Pages/Product/Eligible/Create.vue
@@ -74,6 +74,7 @@
                                 />
                             </div>
                             <button 
+                                v-if="index > 0"
                                 type="button" 
                                 @click="removeProduct(index)"
                                 class="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-lg text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors duration-200"
