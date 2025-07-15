8cd483885e67d93891d598811f13654c88a7f15b
abdirahmanburyar <buryar313@gmail.com>
2025-07-15 05:15:07 +0300

    products upload excel

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -3,12 +3,13 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsHost: import.meta.env.VITE_REVERB_HOST,   // warehouse.psivista.com
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,    // will be 443 here
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,  // also 443
+    forceTLS: true,   // because scheme = https
     enabledTransports: ['ws', 'wss'],
 });
