7afd098efdbec9279a8ca5dfcd22f9a80be731ae
abdirahmanburyar <buryar313@gmail.com>
2025-07-19 17:49:56 +0300

    receive back orders

--- resources/js/Pages/Supplies/BackOrder.vue
+++ resources/js/Pages/Supplies/BackOrder.vue
@@ -138,7 +138,7 @@
                                                     <td class="px-6 py-3 text-sm border-r border-black">
                                                         <div class="flex gap-2">
                                                             <button
-                                                                v-if="row.status === 'Missing' || row.status === 'Damaged' ||  row.status === 'Lost' || row.status === 'Expired' || row.status === 'Low quality'"
+                                                                v-if="row.status === 'Missing'"
                                                                 @click="handleAction('Receive', { ...item, status: row.status, quantity: row.quantity })"
                                                                 class="px-3 py-1.5 text-sm font-medium text-white bg-green-600 rounded hover:bg-green-700"
                                                                 :disabled="isLoading">
