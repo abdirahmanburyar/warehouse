7185c45963c4eb50dbce360a34502a7e1134e7ad
abdirahmanburyar <buryar313@gmail.com>
2025-09-14 20:08:35 +0300

    updated

--- resources/js/Pages/MohInventory/Index.vue
+++ resources/js/Pages/MohInventory/Index.vue
@@ -1507,12 +1507,12 @@ const filteredInventoryItems = computed(() => {
                             </div>
 
                             <div class="flex-1 overflow-auto">
-                                <table class="min-w-full divide-y divide-gray-200" style="min-width: 1300px;">
+                                <table class="min-w-full divide-y divide-gray-200" style="min-width: 1600px;">
                                     <thead class="bg-gray-50 sticky top-0">
                                         <tr>
-                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 300px;">Item *</th>
-                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200 w-32">UoM</th>
-                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200 w-40">Source</th>
+                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 450px;">Item *</th>
+                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 150px;">UoM</th>
+                                            <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 250px;">Source</th>
                                             <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200 w-24">Quantity *</th>
                                             <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 200px;">Batch & Expiry</th>
                                             <th class="px-3 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-r border-gray-200" style="width: 300px;">Warehouse & Location *</th>
@@ -1523,7 +1523,7 @@ const filteredInventoryItems = computed(() => {
                                     <tbody class="bg-white divide-y divide-gray-200">
                                         <tr v-for="(item, index) in createItems" :key="index" class="hover:bg-gray-50">
                                             <!-- Item (Product) -->
-                                            <td class="px-3 py-2 border-r border-gray-200" style="width: 300px;">
+                                            <td class="px-3 py-2 border-r border-gray-200" style="width: 450px;">
                                                 <select v-model="item.product_id" required
                                                     class="w-full px-2 py-1 text-sm border border-gray-300 rounded focus:ring-blue-500 focus:border-blue-500">
                                                     <option value="">Select Product</option>
@@ -1534,13 +1534,13 @@ const filteredInventoryItems = computed(() => {
                                             </td>
 
                                             <!-- UoM -->
-                                            <td class="px-3 py-2 border-r border-gray-200 w-32">
+                                            <td class="px-3 py-2 border-r border-gray-200" style="width: 150px;">
                                                 <input v-model="item.uom" type="text"
                                                     class="w-full px-2 py-1 text-sm border border-gray-300 rounded focus:ring-blue-500 focus:border-blue-500">
                                             </td>
 
                                             <!-- Source -->
-                                            <td class="px-3 py-2 border-r border-gray-200 w-40">
+                                            <td class="px-3 py-2 border-r border-gray-200" style="width: 250px;">
                                                 <input v-model="item.source" type="text"
                                                     class="w-full px-2 py-1 text-sm border border-gray-300 rounded focus:ring-blue-500 focus:border-blue-500">
                                             </td>
